6ac34053a5f8d6b03c73128ae4225eb3ebf76211
Boboqand <Boboqandturdiqulov17@gmail.com>
2022-11-06 16:05:31 +0500

    else error+

--- main.js
+++ main.js
@@ -2,38 +2,52 @@
 
 // ---- 1 ----
 
-var sm = prompt('uzunlikni santimetrda kiriting:');
-var metr = 100;
-var xisob = sm - sm % metr ;
-var result = xisob /metr
-alert('sizning uzunligingizda '+ result + 'ta metr bor');
+// var sm = prompt('uzunlikni santimetrda kiriting:');
+// if(sm > 0){
+//     var metr = 100;
+//     var xisob = sm - sm % metr ;
+//     var result = xisob /metr
+//     alert('sizning uzunligingizda '+ result + 'ta metr bor');
+// }else{
+//     alert('notogri malumot kiritdingiz ');
+// }
 
 
 //  ---- 2 ----
 
 // var kg = prompt('ogirlikni kg da kiriting:');
+// if(kg > 0){
 // var tonna = 1000;
 // var xisob = kg - kg % tonna ;
 // var result = xisob /tonna 
 // alert('siz kiritgan ogirlikda '+ result + 'ta tonna bor');
+// }else{
+//     alert('notogri malumot kiritdingiz ');
+// }
 
 
 //  ---- 3 ----
 
 // var a = prompt('katta kesmani uzunligini kiriting');
 // var b = prompt('kichik kesma uzunligini kiriting');
+// if(b > 0){
 // var x = a - a % b;
 // var result = x / b;
 // alert('katta kesmani ichiga ' + result + ' ta kichik kesmadan joylash mumkun');
-
+// }else{
+//     alert('notogri malumot kiritdingiz ');
+// }
 
 //  ---- 4 ----
 
 // var a = prompt('ikki xonali son kiriting');
+// if(a > 0){
 // var b = a % 10;
 // var x = (a - b)/10;
 // alert('ikki xonali sonning birlik xonasi raqami '+ b +' onliklar xonasidagi raqami '+ x );
-
+// }else{
+//     alert('notogri malumot kiritdingiz ');
+// }
 
 //  ---- 5 ----
 
@@ -56,20 +70,27 @@ alert('sizning uzunligingizda '+ result + 'ta metr bor');
 //  ---- 7 ----
 
 // var a = prompt('uch xonalidan katta son kiriting:');
+// if(a > 0){
 // var xisob = a - a % 100;
 // var result = xisob / 100;
 // alert('siz kiritgan son ichida '+ result + 'ta yuz bor');
+// }else{
+//     alert('notogri malumot kiritdingiz ');
+// }
 
 
 //  ---- 8 ----
 
 // var a = prompt('kun boshidan beri necha sekund utganini kiriting:');
+// if(a > 0){
 // var xisob = a - a % 60;
 // var result = xisob / 60;
 // var b = a - a % 3600;
 // var x = b / 3600; 
 // alert('kun boshidan sizda  '+ result + ' minut yoki ' + x +' soat utibdi');
-
+// }else{
+//     alert('notogri malumot kiritdingiz ');
+// }
 
 //  Mantiqiy amallar (if else)
 
@@ -82,8 +103,10 @@ alert('sizning uzunligingizda '+ result + 'ta metr bor');
 // else if(x < 0){
 //     alert(x + ' soni manfiy');
 // }
-// else{
+// else if(x = 0){
 //     alert(x + ' nolga teng');
+// }else{
+//     alert('notogri malumot kiritdingiz');
 // }
 
 
@@ -95,6 +118,8 @@ alert('sizning uzunligingizda '+ result + 'ta metr bor');
 //     alert(a +' soni juft');
 // }else if(a % 2 != 0){
 //     alert(a + ' soni toq');
+// }else{
+//     alert('notogri malumot kiritdingiz');
 // }
 
 
@@ -112,6 +137,8 @@ alert('sizning uzunligingizda '+ result + 'ta metr bor');
 // }
 // else if(a < 0 && b > 0){
 //     alert('function yolgon');
+// }else{
+//     alert('notogri malumot kiritdingiz');
 // }
 
 
@@ -123,7 +150,7 @@ alert('sizning uzunligingizda '+ result + 'ta metr bor');
 // if( a > b && b > c){
 //     alert('function rost');
 // }else {
-//     alert('function yolgon);
+//     alert('function yolgon');
 // }
 
 
@@ -184,22 +211,22 @@ alert('sizning uzunligingizda '+ result + 'ta metr bor');
 
 //  ---- 10 ----
 
-// var x = prompt('hafta kunlarini raqamda tanlang');
+var x = prompt('hafta kunlarini raqamda tanlang');
 
-// if(x == 1){
-//     alert('Dushanba');
-// }else if(x == 2){
-//     alert('Seshanba');
-// }else if(x == 3){
-//     alert('Shorshanba');
-// }else if(x == 4){
-//     alert('Payshanba');
-// }else if(x == 5){
-//     alert('Juma');
-// }else if(x == 6){
-//     alert('Shanba');
-// }else if(x == 7){
-//     alert('Yakshanba');
-// }else{
-//     alert('siz notogri malumot kritdingiz');
-// }
+if(x == 1){
+    alert('Dushanba');
+}else if(x == 2){
+    alert('Seshanba');
+}else if(x == 3){
+    alert('Shorshanba');
+}else if(x == 4){
+    alert('Payshanba');
+}else if(x == 5){
+    alert('Juma');
+}else if(x == 6){
+    alert('Shanba');
+}else if(x == 7){
+    alert('Yakshanba');
+}else{
+    alert('siz notogri malumot kritdingiz');
+}
