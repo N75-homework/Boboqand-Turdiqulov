2bb0e901d1736e7eeb39f37f52400007a0bd34d1
Boboqand <Boboqandturdiqulov17@gmail.com>
2022-11-09 08:06:43 +0500

    CV N75

--- main.js
+++ main.js
@@ -1,62 +1,48 @@
-var grant = 'Siz tanlagan OTM ga grant kirdingiz';
-var kantrakt = 'Siz tanlagan OTM ga kantraktga kirdingiz';
-var noo = 'Siz tanlagan OTM ga oqishga tavsiya etilmadiz';
-var erro = 'Siz notgri malumot kiritdingiz';
-var firstName = prompt('Ismingizni kiriting');
+var firstName = prompt('Ismingizni kiriting:');
+var lastName = prompt('Familiyangizni kiriting:');
+var number = prompt('Nomeringizni kiriting:');
+var email = prompt('Emailingizni kiriting:');
+var address = prompt('Addressingizni kiriting');
+var scholl = prompt('Nechinchi maktabda oqigansiz');
+var schollNum = prompt('Maktabingizni nechinchi yillarda oqigansiz');
+var Unversy = prompt('Qaysi OTM da tahsil olgansiz');
+var unversyNum = prompt('OTM ni qaysi yillarda oqigansiz');
+var work = prompt('Ayni vaqtda qayerda ishlaysiz');
 
-var OTM = prompt(firstName + ' OTM lar dan birini tanlang: 1.TDIU 2.TDMI 3.MU');
+main.innerHTML = `
+<div class="image"><img src="" alt=""></div>
+<div>
+  <h1 class="firstName" id="firstName">${firstName}</h1>
+  <h1 class="lastName" id="lastName">${lastName}</h1>
+  <div class="item"> 
+    <h3>Phone:</h3>
+    <ins class="number" id="number">${number}</ins>
+  </div>
+  <div class="input">
+    <div>
+      <h3>Email:</h3>
+      <p id="email">${email}</p>
+    </div>
+    <div>
+      <h3>Addres:</h3>
+      <p id="address">${address}</p>  
+    </div>
+  </div>
 
-var ball = prompt( firstName + ' balingizni kiriting:');
+</div>
+<div>
+  
+  <h3>Education</h3>
+  <ins class="schollNum" id="schoolNum">${schoolNum}</ins>
+  <p class="scholl" id="scholl"> ${scholl}</p> 
+  <br>
+  <ins class="unversyNum" id="unversyNum">${unversyNum}</ins>
+  <p class="Unversy" id="Unversy">${Unversy}</p>
+</div>
+<div>
+  <h3>Experience</h3>
+  <p class="work" id="work">${work}</p> 
+</div>
 
-if (OTM == 1){
-    if(ball >= 150){
-        // console.log(grant);
-        alert( grant);
-    }
-    else if( ball > 120){
-        // console.log(kantrakt);
-        alert(kantrakt);
-    }
-    else if(120 >= ball > 0){
-        // console.log(no);
-        alert( noo);
-    }
-    else{
-        alert(erro);
-    }
-}else if(OTM == 2){
-    if(ball >= 160){
-        // console.log(grant);
-        alert(grant);
-    }
-    else if(ball > 100){
-        // console.log(kantrakt);
-        alert(kantrakt);
-    }
-    else if(100 >= ball > 0){
-        // console.log(no);
-        alert( noo);
-    }
-    else{
-        alert(erro);
-    }
-}else if(OTM == 3){
-    if(ball >= 180){
-        // console.log(grant);
-        alert(grant);
-    }
-    else if( ball > 168){
-        // console.log(kantrakt);
-        alert(kantrakt);
-    }
-    else if(168 >= ball > 0){
-        // console.log(no);
-        alert( noo);
-    }
-    else{
-        alert(erro);
-    }
-}else{
-    alert(erro);
-}
+`
 
